cc8071c8afd2804c6f66ba74f3f64894241dfd16
Robson Natanael <natanaelrobson@gmail.com>
2022-04-02 20:24:35 -0300

    style: adjust background color the body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -32,7 +32,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: var(---background);
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 
