1dcac303708ebae3bda30efe47a82ee2fabe521c
Robson Natanael <natanaelrobson@gmail.com>
2022-04-02 22:14:20 -0300

    feat: create components
    dashboard, header, summary and transactions table

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>02/04/2022</td>
+          </tr>
+          <tr>
+            <td>Financiamento Imobiliário</td>
+            <td className="withdraw">- R$700</td>
+            <td>Financiamento</td>
+            <td>05/04/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
